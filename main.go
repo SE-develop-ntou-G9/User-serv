74aabb9742ed6bfdc352aaad49d6e691413a5c9d
unknown <liuchangyan1014@gmail.com>
2025-11-19 21:03:20 +0800

    add CORS

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"golangAPI/repositories"
 	"golangAPI/usecase"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db, err := infrastructure.NewDBInstance()
+
 	if err != nil {
 		panic("Failed to connect to database")
 	}
@@ -21,6 +23,8 @@ func main() {
 
 	router := gin.Default()
 	v1 := router.Group("/v1")
+
+	router.Use(cors.Default())
 
 	authUC := usecase.NewAuthUsecase(userRepo) // Assuming you have a userRepo instance
 	delivHTTP.RegisterAuthRoutes(v1, authUC)
