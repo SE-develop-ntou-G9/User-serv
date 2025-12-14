d7eb2a79fda0eae91540e3413dd9158f7f07ad96
unknown <liuchangyan1014@gmail.com>
2025-12-14 20:26:27 +0800

    add function for user notification

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 
 	userRepo := repositories.NewUserRepository(db)
 	imageRepo := repositories.NewImageRepository(s3client)
+	notifyRepo := repositories.NewNotifyRepository(db)
 
 	router := gin.Default()
 	v1 := router.Group("/v1")
@@ -30,9 +31,11 @@ func main() {
 	authUC := usecase.NewAuthUsecase(userRepo) // Assuming you have a userRepo instance
 	userUC := usecase.NewUserUsecase(userRepo)
 	imageUC := usecase.NewImageUsecase(imageRepo)
+	notifyUC := usecase.NewNotifyUsecase(notifyRepo)
 	delivHTTP.RegisterAuthRoutes(v1, authUC)
 	delivHTTP.RegisterUserRoutes(v1, userUC)
 	delivHTTP.RegisterImageRoutes(v1, imageUC)
+	delivHTTP.RegisterNotifyRoutes(v1, notifyUC)
 
 	router.Run(":8080")
 }
